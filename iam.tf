8e75665f5f6d6434fe3f7c6133968f4b4d4ac3cc
amousa <a_mousa@hotmail.com>
2025-08-01 00:02:09 -0700

    Add files via upload

--- iam.tf
+++ iam.tf
@@ -19,3 +19,28 @@ resource "aws_iam_role_policy_attachment" "fis_role_policy" {
   role       = aws_iam_role.fis_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSFaultInjectionServiceRolePolicy"
 }
+
+#------------------------------------------------------
+# AWSFaultInjectionServiceRolePolicy managed policy
+#------------------------------------------------------
+resource "aws_iam_role" "experiment_runner" {
+  name = "experiment-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "fis.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "experiment_runner_policy" {
+  role       = aws_iam_role.experiment_runner.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSFaultInjectionServiceRolePolicy"
+}
